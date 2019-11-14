2db6d9872115791699a7512d1ddabbe316e86c50
chipomwitu <chipomwitu88@gmail.com>
2019-11-14 09:12:18 +0300

    fixed syntax error on schema.sql

--- app-test.py
+++ app-test.py
@@ -1,6 +1,8 @@
+import os
+import unittest
+
 from app import app
 
-import unittest
 
 class BasicTestCase(unittest.TestCase):
     
@@ -8,6 +10,10 @@ class BasicTestCase(unittest.TestCase):
         tester=app.test_client(self)
         response=tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 404)
+    
+    def test_database(self):
+        tester=os.path.exists('flaskr.db')
+        self.assertTrue(tester)
 
     
     if __name__=='__main__':
--- app.py
+++ app.py
@@ -20,9 +20,35 @@ SECRET_KEY='iamamasterflasker'
 USERNAME='admin'
 PASSWORD='admin'
 
-
 app=Flask(__name__)
 app.config.from_object(__name__)
+
+
+# connect to the database
+def connect_db():
+    rv=sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory=sqlite3.Row
+    return rv
+
+# create the database
+def init_db():
+    with app.app_context():
+        db=get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# open database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db=connect_db()
+    return g.sqlite_db
+
+# close database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 #@app.route('/')
@@ -31,4 +57,5 @@ app.config.from_object(__name__)
 
 
 if __name__=='__main__':
+    init_db()
     app.run()

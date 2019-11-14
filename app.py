f1d134d9b14f4c596d7c79a88fe65ef40b4af5b3
chipomwitu <chipomwitu88@gmail.com>
2019-11-14 10:45:32 +0300

    added logic and page to show index of entries

--- app.py
+++ app.py
@@ -51,9 +51,13 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-#@app.route('/')
-#def hello():
-#    return 'Hello, Master Flasker!'
+@app.route('/')
+def show_entries():
+    '''Searches the database for entries then displays them.'''
+    db=get_db()
+    cur=db.execute('select * from entries order by id desc')
+    entries=cur.fetchall()
+    return render_template('index.html', entries=entries)
 
 
 if __name__=='__main__':
